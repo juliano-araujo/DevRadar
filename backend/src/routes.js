f8eabfd474572495fd65733a1360bff5ee35a3b7
Juliano <julianosilva82010@gmail.com>
2020-02-11 00:03:27 -0400

    style(backend): organize routes style

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,7 +5,8 @@ const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.route('/devs').post(DevController.store)
+routes.route('/devs')
+  .post(DevController.store)
   .get(DevController.index);
 
 routes.route('/search').get(SearchController.index);
